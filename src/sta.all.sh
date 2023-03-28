15560ba09d94eca60ee503707beab5d82a2e9538
Eric Smith <eric@Erics-M1.local>
2023-03-28 12:03:49 -0700

    fix typos

--- src/sta.all.sh
+++ src/sta.all.sh
@@ -7,15 +7,15 @@ run_sta()
 {
     # Get the STA results from a single openlane run
     pwd
-    sta -no_splash -exit ../src/sta.ring.tcl 2>null | grep -v Warn
+    sta -no_splash -exit /foss/designs/tt02-verilog-ring-osc-demo/src/sta.ring.tcl 2>null | grep -v Warn
 }
-cd 
+
 for d in `find  . -maxdepth 1 -name "RUN_*" -a -type d`
 do
     cd $d
     # Just grab the text we care about and format it into a csv
     run_sta | awk '$1 ~ /RUN_/ {r=$1}; \
-                   $1 ~ /ring0/ {r0n=$1;r0v=$4}; 
+                   $1 ~ /ring0/ {r0n=$1;r0v=$4}; \
                    $1 ~ /ring1/ {print r "," r0n "," r0v "," $1 "," $4} '
     cd ..
 done
